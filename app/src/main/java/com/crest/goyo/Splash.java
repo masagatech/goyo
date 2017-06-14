c3088f07336ae50e6ac4e1f95877eba6fc3d93e8
fajar <fajarpm786@gmail.com>
2017-06-14 12:20:34 +0530

    14-Jun Added Crash Reporting.

--- app/src/main/java/com/crest/goyo/Splash.java
+++ app/src/main/java/com/crest/goyo/Splash.java
@@ -7,6 +7,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 
 import com.crest.goyo.Utils.Preferences;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class Splash extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
@@ -16,6 +18,7 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         getSupportActionBar().hide();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash);
